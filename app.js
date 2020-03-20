757923569b2a8ed04e6f6b2198fe7e0344e8ed0d
AlenS <alenskupnjak@yahoo.com>
2020-03-20 15:27:57 +0100

    004- ubacen HTML

--- app.js
+++ app.js
@@ -53,12 +53,15 @@ let UIController = (function(){
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensContainer:'.expenses__list'
+
   }
 
 
 
-  // veza sa vanjskim svijetom
+  // veza sa vanjskim svijetom, tu se sve funkcije
   return {
     getinput: function(){
       return {
@@ -69,6 +72,26 @@ let UIController = (function(){
     },
     getDOMstrings: function(){
       return DOMstring;
+    },
+    addlistItem:function(obj,type){
+      let html, newHtml;
+      // create HTML with placew hoker text
+      if ( type === 'inc') {
+        element = DOMstring.incomeContainer;
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      } else if (type === 'exp') {
+        element = DOMstring.expensContainer;
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      }
+
+      // Replace the place holder text
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+
+      // insert the HTML into the doom
+        let set = document.querySelector('.income__list');
+        document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
     }
   }
 })();
@@ -95,13 +118,14 @@ let controller = (function(budgetCtrl, UICtrl) {
     let input, newItem;
 
     // 1. Očitaj uneseni podatak
-    let input = UICtrl.getinput();
+    input = UICtrl.getinput();
 
     // 2. dodaj vrijednost na budget conntroller
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-    console.log(item)
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);     console.log(newItem)
 
     // 3. dodaj vrijednost na UI
+    diplay = UICtrl.addlistItem(newItem,input.type)
+    
     // 4. izračunaj budget
     // 5. Ispisati budget na UI 
   }
