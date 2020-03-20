5333d8a352abbca4e77e5d0f1066ee79d7917874
AlenS <alenskupnjak@yahoo.com>
2020-03-20 09:57:37 +0100

    002 dodan očitanje ENTER ili button ad

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ return {
 budgetController.publicTest(5)
 
 
-// UI CONTROLER, ispisivanje na ekran
+// UI CONTROLER
 let UIController = (function(){
 
 
@@ -29,8 +29,28 @@ let UIController = (function(){
 // GLAVNA KONZOLA ZA UPRAVLJANJU
 let controller = (function(budgetCtrl, UICtrl) {
 
-let z =budgetCtrl.publicTest(5)
+let ctrlAddItem = function() {
+  console.log('evo me')
+  // 1. Očitaj uneseni podatak
+  // 2. dodaj vrijednost na budget conntroller
+  // 3. dodaj vrijednost na UI
+  // 4. izračunaj budget
+  // 5. Ispisati budget na UI 
+}
 
+// Pokreče program
+let a = document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+// registrira stiskanje bilo koje tipke, mi trazimo enter!
+document.addEventListener('keypress', function(event){
+  if (event.keyCode === 13 || event.which === 13) {
+   ctrlAddItem();
+  }
+});
+
+
+
+let z =budgetCtrl.publicTest(5)
 return {
   anotherPublic: function() {
     console.log(z);
